c04cb118a6577e1ab81f8ff9528f2ec6c9e93752
Tahm24 <156429364+Tahm24@users.noreply.github.com>
2024-10-15 12:56:59 +0100

    v3

--- routes/main.js
+++ routes/main.js
@@ -2,6 +2,16 @@
 const express = require("express")
 const router = express.Router()
 
+
+//red
+const redirectLogin = (req, res, next) => {
+    if (!req.session.userId ) {
+      res.redirect('../users/login') // redirect to the login page
+    } else { 
+        next (); // move to the next middleware function
+    } 
+}
+
 // Handle our routes
 router.get('/',function(req, res, next){
     res.render('index.ejs')
